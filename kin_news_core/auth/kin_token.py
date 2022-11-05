f569b99dc8ce7e15de729c11a6e1c0d3ac2ed1bb
kinfi4 <ilyamakarov223@gmail.com>
2022-11-05 23:36:30 +0200

    Another aproach for Kin Tokens authorization chosed

--- kin_news_core/auth/kin_token.py
+++ kin_news_core/auth/kin_token.py
@@ -4,23 +4,11 @@ from rest_framework.authentication import BaseAuthentication, get_authorization_
 from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 from rest_framework.request import Request
 
-from kin_news_core.constants import KIN_TOKEN_PREFIX
-
 
 class KinTokenAuthentication(BaseAuthentication):
     def authenticate(self, request: Request):
-        token_header = get_authorization_header(request).split()  # list of "Token" prefix and token value
+        kin_token = request.data.dict().get('token')
 
-        if not token_header:
-            raise NotAuthenticated('Provide authentication header')
-        if token_header[0].decode('utf-8').lower() != KIN_TOKEN_PREFIX:
-            raise AuthenticationFailed('Invalid authentication token provided')
-        if len(token_header) >= 3:
-            raise AuthenticationFailed(detail='Authentication header has spaces')
-        if len(token_header) == 1:
-            raise AuthenticationFailed(detail='Authentication header does not contains token')
-
-        kin_token = self._decode_token_from_header(token_header)
         if kin_token != settings.KIN_TOKEN:
             raise AuthenticationFailed('Invalid authentication token provided')
 
