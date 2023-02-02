df39ba31337c872573d7d63975cb1a75a628df68
kinfi4 <ilyamakarov223@gmail.com>
2023-02-02 22:10:14 +0200

    Kin Token added to authentication options

--- kin_news_core/auth/__init__.py
+++ kin_news_core/auth/__init__.py
@@ -1,1 +1,2 @@
 from .jwt import decode_jwt_token, create_jwt_token
+from .kin_token import decode_kin_token
---
+++ kin_news_core/auth/kin_token.py
@@ -0,0 +1,10 @@
+from kin_news_core.exceptions import AuthenticationFailedError
+
+
+def decode_kin_token(header: str) -> str:
+    split_token = header.split()
+
+    if len(split_token) != 2:
+        raise AuthenticationFailedError('Invalid token format!')
+
+    return split_token[1]
