db5dcab33e8c65c4b0de3f94c04f6e4edfcbad65
kinfi4 <ilyamakarov223@gmail.com>
2023-02-06 21:09:59 +0200

    Improved serialization for rabbitmq

--- kin_news_core/messaging/common/serializers.py
+++ kin_news_core/messaging/common/serializers.py
@@ -1,8 +1,11 @@
 import json
 from typing import Type
+from copy import deepcopy
+from datetime import date, datetime
 
 from kin_news_core.messaging.dtos.event import BasicEvent
 from kin_news_core.messaging.interfaces import ISerializer, IDeserializer
+from kin_news_core.constants import DEFAULT_DATE_FORMAT
 
 
 class JsonSerializer(IDeserializer, ISerializer):
@@ -10,4 +13,9 @@ class JsonSerializer(IDeserializer, ISerializer):
         return event_class(**json.loads(data.decode()))
 
     def serialize(self, event: BasicEvent) -> bytes:
-        return event.json().encode()
+        data_to_encode = deepcopy(event.dict())
+        for key, value in event.dict().items():
+            if isinstance(value, (datetime, date)):
+                data_to_encode[key] = value.strftime(DEFAULT_DATE_FORMAT)
+
+        return json.dumps(data_to_encode).encode()
