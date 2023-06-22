b9611234cc0e6381eac118afbae01af99f01a5d3
kinfi4 <ilyamakarov223@gmail.com>
2023-06-22 09:19:09 +0300

    Async telegram interface fixed.

--- kin_news_core/settings.py
+++ kin_news_core/settings.py
@@ -11,12 +11,19 @@ class PostgresSettings(BaseSettings):
 
 class CoreSettings(BaseSettings):
     secret_key: str = Field(..., env="SECRET_KEY")
-    token_life_minutes: int = Field(..., env='TOKEN_LIFE_MINUTES')
+    token_life_minutes: int = Field(..., env="TOKEN_LIFE_MINUTES")
 
     postgres: PostgresSettings = PostgresSettings()
 
 
 class TelegramSettings(BaseSettings):
-    api_id: int = Field(..., env='TELEGRAM_API_ID')
-    api_hash: str = Field(..., env='TELEGRAM_API_HASH')
-    session_string: str = Field(..., env='TELEGRAM_SESSION_STRING')
+    api_id: int = Field(..., env="TELEGRAM_API_ID")
+    api_hash: str = Field(..., env="TELEGRAM_API_HASH")
+    session_string: str = Field(..., env="TELEGRAM_SESSION_STRING")
+
+
+class RedisSettings(BaseSettings):
+    host: str = Field(..., env="REDIS_HOST")
+    port: int = Field(6379, env="REDIS_PORT")
+    photo_db_name: int = Field(None, env="REDIS_PHOTO_DB_NAME")
+    channel_db_name: int = Field(None, env="REDIS_CHANNELS_DB")
--- kin_news_core/telegram/client.py
+++ kin_news_core/telegram/client.py
@@ -6,7 +6,6 @@ from typing import Optional
 from telethon import TelegramClient
 from telethon.errors import FloodWaitError, UsernameInvalidError
 from telethon.tl.custom.message import Message
-from telethon.tl.types import Channel
 from telethon.sessions import StringSession
 from telethon import functions
 
@@ -14,7 +13,7 @@ from kin_news_core.exceptions import InvalidChannelURLError, TelegramIsUnavailab
 from kin_news_core.telegram.entities import TelegramMessageEntity, TelegramChannelEntity
 from kin_news_core.constants import MESSAGES_LIMIT_FOR_ONE_CALL
 from kin_news_core.telegram.interfaces import IDataGetterProxy
-
+from kin_news_core.telegram.retry import retry_connection_async, retry_connection_sync
 
 logging.getLogger("telethon").setLevel(logging.ERROR)
 
@@ -28,6 +27,7 @@ class TelegramClientProxy(IDataGetterProxy):
         self._client = None
         self._logger = logging.getLogger(self.__class__.__name__)
 
+    @retry_connection_sync
     def fetch_posts_from_channel(
         self,
         channel_name: str,
@@ -36,7 +36,6 @@ class TelegramClientProxy(IDataGetterProxy):
         earliest_date: Optional[datetime] = None,
         skip_messages_without_text: bool = False,
     ) -> list[TelegramMessageEntity]:
-        self._client = self._initialize_client()
         with self._client:
             try:
                 return self._client.loop.run_until_complete(
@@ -51,22 +50,23 @@ class TelegramClientProxy(IDataGetterProxy):
                 self._logger.error(f"Telegram flood happened, backoff: {error.seconds}")
                 raise TelegramIsUnavailable("Telegram flooded!", seconds=error.seconds)
 
+    @retry_connection_sync
     def get_channel(self, channel_link: str) -> TelegramChannelEntity:
         self._logger.info(f"[TelegramClientProxy] Getting information for channel: {channel_link}")
 
-        self._client = self._initialize_client()
         with self._client:
             return self._client.loop.run_until_complete(
                 self.get_channel_async(channel_link)
             )
 
+    @retry_connection_sync
     def download_channel_profile_photo(self, channel_link: str, path_to_save: str) -> None:
-        self._client = self._initialize_client()
         with self._client:
             self._client.loop.run_until_complete(self.download_channel_profile_photo_async(
                 channel_link, path_to_save
             ))
 
+    @retry_connection_async
     async def fetch_posts_from_channel_async(
         self,
         channel_name: str,
@@ -77,42 +77,46 @@ class TelegramClientProxy(IDataGetterProxy):
     ) -> list[TelegramMessageEntity]:
         self._logger.info(f"[TelegramProxy] Fetching data from {channel_name}")
 
-        channel, _, _ = await self.get_channel_async(channel_name)
+        channel_entity: TelegramChannelEntity = await self.get_channel_async(channel_name)
         messages_to_return = []
 
         previous_message: Optional[Message] = None
         message: Message
-        async for message in self._client.iter_messages(
-            channel,
-            offset_date=offset_date,
-            limit=MESSAGES_LIMIT_FOR_ONE_CALL,
-        ):
-            if (
-                previous_message is not None
-                and (previous_message.date == message.date or previous_message.text == message.text)
+
+        async with self._client:
+            async for message in self._client.iter_messages(
+                channel_entity.link,
+                offset_date=offset_date,
+                limit=MESSAGES_LIMIT_FOR_ONE_CALL,
             ):
-                continue
+                if (
+                    previous_message is not None
+                    and (previous_message.date == message.date or previous_message.text == message.text)
+                ):
+                    continue
 
-            if skip_messages_without_text and not message.text:
-                continue
+                if skip_messages_without_text and not message.text:
+                    continue
 
-            local = datetime.now().tzinfo
-            post_local_date = message.date.astimezone(tz=local)
-            post_local_date = datetime.fromtimestamp(post_local_date.timestamp())
+                local = datetime.now().tzinfo
+                post_local_date = message.date.astimezone(tz=local)
+                post_local_date = datetime.fromtimestamp(post_local_date.timestamp())
 
-            if earliest_date and post_local_date < earliest_date:
-                break
+                if earliest_date and post_local_date < earliest_date:
+                    break
 
-            messages_to_return.append(message)
-            previous_message = message
+                messages_to_return.append(message)
+                previous_message = message
 
         return [TelegramMessageEntity.from_telegram_obj(msg) for msg in messages_to_return]
 
+    @retry_connection_async
     async def get_channel_async(self, channel_link: str) -> TelegramChannelEntity:
         try:
-            channel_full_obj = await self._client(functions.channels.GetFullChannelRequest(channel=channel_link))
+            async with self._client:
+                channel_full_obj = await self._client(functions.channels.GetFullChannelRequest(channel=channel_link))
         except (ValueError, TypeError, UsernameInvalidError) as err:
-            self._logger.warning(f"Impossible to find channel for {channel_link}, with error: {str(err)}")
+            self._logger.warning(f"Impossible to find channel for {channel_link}, with {err.__class__.__name__}: {str(err)}")
             raise InvalidChannelURLError(f"Channel link {channel_link} is invalid, or channel with this name does not exists")
 
         return TelegramChannelEntity.from_telegram_obj(
@@ -121,9 +125,11 @@ class TelegramClientProxy(IDataGetterProxy):
             channel_full_obj.full_chat.participants_count,
         )
 
+    @retry_connection_async
     async def download_channel_profile_photo_async(self, channel_link: str, path_to_save: str) -> None:
         try:
-            await self._client.download_profile_photo(channel_link, file=path_to_save)
+            async with self._client:
+                await self._client.download_profile_photo(channel_link, file=path_to_save)
         except ValueError as err:
             self._logger.warning(f"Impossible to find channel for {channel_link}, with error: {str(err)}")
             raise InvalidChannelURLError(f"Channel link {channel_link} is invalid, or channel with this name does not exists")
