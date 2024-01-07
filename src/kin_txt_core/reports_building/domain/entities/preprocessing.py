bb9ce502946eedb620ce3e7ed66a62aa1b170b8f
kinfi4 <ilyamakarov223@gmail.com>
2024-01-07 11:39:27 +0200

    [CORE] Update preprocessing config objevt

--- src/kin_txt_core/reports_building/domain/entities/preprocessing.py
+++ src/kin_txt_core/reports_building/domain/entities/preprocessing.py
@@ -4,9 +4,13 @@ from pydantic import BaseModel, Field
 class PreprocessingConfig(BaseModel):
     lowercase: bool = Field(default=True)
     remove_links: bool = Field(default=True, alias="removeLinks")
-    remove_emoji: bool = Field(default=True, alias="removeEmoji")
+    remove_emoji: bool = Field(default=False, alias="removeEmoji")
     remove_punctuation: bool = Field(default=True, alias="removePunctuation")
     remove_extra_spaces: bool = Field(default=True, alias="removeExtraSpaces")
     remove_html_tags: bool = Field(default=True, alias="removeHtmlTags")
 
-    remove_stop_words: bool = Field(default=True, alias="removeStopWords")
+    remove_stop_words: bool = Field(default=False, alias="removeStopWords")
+    stop_words_file_original_name: str | None = Field(default=None, alias="stopWordsFileOriginalName")
+
+    class Config:
+        allow_population_by_field_name = True
